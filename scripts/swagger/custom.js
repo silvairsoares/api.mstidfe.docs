cbbcbb246f0dd1b53bf9213c81a369c2672e3072
Silvair L. Soares <silvairsoares@outlook.com>
2019-11-01 14:19:41 -0300

    Add files via upload

---
+++ scripts/swagger/custom.js
@@ -0,0 +1,35 @@
+﻿//var teste = document.querySelectorAll("a");//.style.border = "10px solid red";
+
+//var linkNFeSimplesNacional = findElements("a", "http://endereconfevarejo.com.br");
+
+//linkNFeSimplesNacional.addEventListener("click", preencheModeloSimplesNacional, false); 
+
+//function preencheModeloSimplesNacional() {
+//    alert('Olá');
+//}
+
+////(function () {
+//    var logo = document.getElementsByTagName('img');
+//    logo.href = "https://www.foo.com";
+
+//    var logoImg = document.getElementsByTagName('img');
+
+//    logoImg = undefined;
+//    logoImg.alt = "Foo, Inc.";
+//    logoImg.src = "/images/logo.jpg";
+//    logoImg.width = 496;
+////})();
+
+
+//function findElements(selector, text) {
+//    var anchors = document.querySelectorAll(selector);
+//    var selectedLinks = [];
+
+//    for (var i = 0; i < anchors.length; i++) {
+//        var link = anchors[i];
+
+//        if (link.innerHTML === text) {
+//            selectedLinks.push(link);
+//        }
+//    }
+//}
